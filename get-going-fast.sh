f84b1d051a12a44835bca232fdf61fb470d632c5
Ben Letchford <contact@benl.com.au>
2018-06-06 21:50:15 +1000

    Get going fast script

---
+++ get-going-fast.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+REQUIRED_PACKAGES=( ansible git wget )
+for i in "${REQUIRED_PACKAGES[@]}"
+do
+  echo -n "Checking if $i installed... "
+  if [ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+    echo "installing"
+    apt-get install $i;
+  else
+    echo "ok"
+  fi
+done
+
+_USER=${SUDO_USER:-$USER}
+mkdir -p /home/$_USER/repos
+
+if [ -d "/home/$_USER/repos/my-ansible" ]; then
+  pushd /home/$_USER/repos/my-ansible
+  git checkout master && git pull origin master
+else
+  pushd /home/$_USER/repos
+  git clone https://github.com/benletchford/my-ansible
+  pushd my-ansible
+fi
+
+chown -R $_USER:$_USER /home/$_USER/repos
+
+ansible-playbook -i "localhost," -c local playbook.yml
